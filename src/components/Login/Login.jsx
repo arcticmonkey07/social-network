59aac71112c5f270a2d3e120af96be2057f22f26
arcticmonkey07 <arcticmonkey07@gmail.com>
2019-08-29 21:30:14 +0300

    75 - React JS - redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import './Login.css';
+import {Field, reduxForm} from "redux-form";
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit} className='login__form form'>
+      <Field
+        type="text"
+        name={'login'}
+        className='form__input'
+        placeholder={'Login'}
+        component={'input'}
+      />
+      <Field
+        type="text"
+        name={'password'}
+        className='form__input'
+        placeholder={'Password'}
+        component={'input'}
+      />
+      <label className='form__input'>
+        <Field
+          type="checkbox"
+          name={'rememberMe'}
+          component={'input'}
+        />Remember me
+      </label>
+      <button className='form__sumbit'>Login</button>
+    </form>
+  )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = (props) => {
-  return <h1>Login</h1>
+  const onSubmit = (formData) => {
+    console.log(formData);
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+  )
 }
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,14 +4,16 @@ import dialogsReducer from './dialogs-reducer.js';
 import sidebarReducer from './sidebar-reducer.js';
 import usersReducer from './users-reducer.js';
 import authReducer from "./auth-reducer.js";
-import thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
